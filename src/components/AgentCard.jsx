071aaef10ff0b9ba18a73f2c7c7646d85e97668c
AlbertCastellon <albertkstyon@gmail.com>
2025-01-05 16:17:08 +0100

    initial commit

---
+++ src/components/AgentCard.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+const AgentCard = ({ agent }) => {
+    if(!agent.role){
+        return
+    }else{
+    return (
+        <div className="agentCard">
+            <img className="icon" src={agent.displayIcon} alt="icono" />
+            <div className="footnote">
+                <div>
+                    <h2>{agent.displayName}</h2>
+                    <h4>{agent.role.displayName}</h4>
+                </div>
+                <div className={`role ${agent.role.displayName}`}></div>
+            </div>     
+        </div>
+    )
+}
+}
+
+export default AgentCard
